877fb8a00690ce0464711829d3ab01036f14db4e
TheJB4 <juanmer382@gmail.com>
2022-07-15 14:15:38 -0300

    component Register user

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,153 @@
+import React,{useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Formik, Form, Field , ErrorMessage } from 'formik';
+
+const RegisterForm = () => {
+  let [sendForm,setSendForm] = useState(false)
+  return (
+    <>
+      <h1>Sing in into your count</h1>
+      <Formik 
+      initialValues={
+        {
+          firstName: '',
+          lastName: '',
+          email:'',
+          password:''
+        }
+      }
+
+      validate={(values)=>{
+        let {firstName, lastName, email , password} = values
+
+        let myErrors = {}
+
+        //Validate a name:
+        if(!firstName){
+          myErrors.firstName = "Please insert a name"
+        }else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(firstName)){
+          myErrors.firstName = "The name can only contain letters and spaces"
+        }
+
+        //Validate a lastName:
+        if(!lastName){
+          myErrors.lastName = "Please insert a Last name"
+        }else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(lastName)){
+          myErrors.lastName = "The Last Name can only contain letters and spaces"
+        }
+
+        //Validate email:
+        if(!email){
+          myErrors.email = "Please insert a email"
+        }else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(email)){
+          myErrors.email = "The email can only contain letters numbers dots"
+        }
+
+        //validate a password:
+        if(!password){
+          myErrors.password = "Please insert a password"
+        }else if(password.length < 6){
+          myErrors.password = "Password must have at least 6 characters."
+        }
+
+        return myErrors
+      }}
+
+      onSubmit={(values, {resetForm})=>{
+        
+        resetForm()
+        //Aqui va la peticion fetch para crear el usuario en la base de datos EJ:
+        /*
+        fetch('/users/auth/register', {
+          method: 'POST',
+          headers:{
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(values)
+        })
+        */
+        setSendForm(true)
+        setTimeout(()=>{
+          setSendForm(false)
+        },3000)
+      }}>
+
+        { ( {errors,touched} ) =>(
+          <Form className='d-lg-flex flex-lg-column justify-content-lg-center align-items-lg-center border w-50 fw-bold mt-20'>
+            <div className="mb-3 w-50">
+              <label htmlFor="firstName" className="form-label">
+                First Name
+              </label>
+              <Field
+                type="text"
+                className="form-control"
+                id="firstName"
+                name='firstName'
+              />
+              {touched.firstName && errors.firstName && <div className='alert alert-danger' role='alert'>{errors.firstName}</div>}
+            </div>
+            <div className="mb-3 w-50">
+              <label htmlFor="lastName" className="form-label">
+                Last Name
+              </label>
+              <Field
+                type="text"
+                className="form-control"
+                id="lastName"
+                name='lastName'
+              />
+              {touched.lastName && errors.lastName && <div className='alert alert-danger' role='alert'>{errors.lastName}</div>}
+            </div>
+            <div className="mb-3 w-50">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <Field
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                name='email'
+              />
+             <ErrorMessage name='email' component={()=>{
+                return (<div className='alert alert-danger' role='alert'>{errors.email}</div>)
+              }}/>
+            </div>
+            <div className="mb-3 w-50">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <Field
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                name='password'
+              />
+              <ErrorMessage name='password' component={()=>{
+                return (<div className='alert alert-danger' role='alert'>{errors.password}</div>)
+              }}/>
+            </div>
+            <div className="mb-3 w-60 form-check">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                Remember me
+              </label>
+              <a href="/" className="form-check-label ms-5">
+                Forgot your password?
+              </a>
+            </div>
+            <button type="submit" className="btn btn-primary w-50 fw-bold">
+              Submit
+            </button>
+            {sendForm && <div className='alert alert-success' role='alert'>Register Success</div> }
+          </Form>
+        )}
+        
+      </Formik>
+     
+    </>
+      
+  );
+}
+
+export default RegisterForm
