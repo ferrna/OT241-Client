f08f115295222fede7216de1481b789ce76e17ba
NicoMunoz909 <nicolas.munoz@outlook.es>
2022-07-19 18:38:21 -0300

    Start styling buttons

--- src/components/Backoffice/MenuButton.jsx
+++ src/components/Backoffice/MenuButton.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const MenuButton = (props) => {
   return (
-    <div>
-      <h2>{props.title}</h2>
-      {props.icon}
-      <a href={props.to}>Ir</a>
+    <div className='container-sm shadow p-3 mb-5 bg-body rounded'>
+      <h2 className='h6 text-center'>{props.title}</h2>
+      <div className='col text-center'>{props.icon}</div>
+      <a className='d-flex justify-content-center bg-primary text-white' href={props.to}>Ir</a>
     </div>
   )
 }
