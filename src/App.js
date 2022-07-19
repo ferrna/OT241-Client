a66ee33c650b08363b78aabe800941f74922697a
CarlosRubio9827 <rubiogallegoc@gmail.com>
2022-07-18 21:47:16 -0500

    Add Footer - OT241-22

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Slider from './components/Slider';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="">
       <Slider/>
+      <Footer/>
     </div>
   );
 }
