c2b103d69eb88f207b44cc2626ec76d24e01b134
NicoMunoz909 <nicolas.munoz@outlook.es>
2022-07-26 23:44:12 -0300

    Create login and logout reducers

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './app/store'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/reducers/authSlice.js
+++ src/reducers/authSlice.js
@@ -1,24 +1,53 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import httpService from '../services/httpService';
+
+const service = new httpService();
+
+export const login = createAsyncThunk('auth/login', async (email, password, thunkAPI) => {
+  try {
+    return await service.post("auth/login", {email, password})
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState: {
     isLoggedIn: false,
-    user: {},
-    error: ''
+    user: null,
+    error: '',
+    isLoading: false
   },
   reducers: {
-    login: state => {
-      state.isLoggedIn = true;
-      state.user = { email: 'email@email.com', password: '123456' };
-    },
     logout: state => {
       state.isLoggedIn = false;
-      state.user = {};
+      state.user = null;
+      state.error= '';
+      state.isLoading= false;
     }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isLoggedIn = true
+        state.user = action.payload
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false
+        state.error = action.payload
+        state.user = null
+      })
   }
 });
 
-export const { login, logout } = authSlice.actions;
-
+export const { logout } = authSlice.actions;
 export default authSlice.reducer;
