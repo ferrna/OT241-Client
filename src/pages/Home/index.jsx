67e2f12b80141109c4843670ed915df37be62686
TheJB4 <juanmer382@gmail.com>
2022-08-02 19:17:12 -0300

    formulario home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
 import Slider from "../../components/Slider.jsx";
 import Novedades from "./Novedades/index.jsx";
@@ -7,10 +7,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import { novedades, homeWelcome } from "./Novedades/mockdata.js";
+import RegisterFormEdit from "../../components/RegisterFormEdit.jsx";
 
+let userIsAdmin = true
+let infoHomeDefault = {
+  title: 'Hola Bienvenidx',
+  description: 'Lorem ipsun dolor anmet'
+}
 function Home(props) {
+  const [infoHome, setInfoHome] = useState(infoHomeDefault)
+  let [images, setImages] = useState({})
   return (
     <>
+    {userIsAdmin && <RegisterFormEdit infoHome={infoHome} images={images} setInfoHome={setInfoHome} setImages={setImages}/>}
       <section className="position-relative" style={{ minHeight: "350px" }}>
         <Slider />
         <div
@@ -18,8 +27,10 @@ function Home(props) {
           style={{ left: "0", right: "0" }}
         >
           <div style={{ maxWidth: "500px" }}>
-            <h2 className="text-center text-sm-start mx-1 my-2 fw-bolder">Hola! Bienvenidx</h2>
-            <p>{homeWelcome && homeWelcome.text}</p>
+            <h2 className="text-center text-sm-start mx-1 my-2 fw-bolder">
+              {infoHome.title}
+            </h2>
+            <p>{homeWelcome && infoHome.description}</p>
           </div>
         </div>
       </section>
