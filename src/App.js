987879539b3bd230e769275e458c098e7cca5b4c
Fernando Arriondo <arriondovfernando@gmail.com>
2022-08-03 03:11:57 -0300

    Backoffice: dynamic menu by role

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import RoutesNav from "./components/RoutesNav";
-import Slider from "./components/Slider";
-import Backoffice from "./components/Backoffice";
 
 function App() {
   return (
     <div className="">
       <Header />
-      <Slider />
-      <Backoffice />
       <RoutesNav />
       <Footer />
     </div>
--- src/components/Backoffice/index.jsx
+++ src/components/Backoffice/index.jsx
@@ -1,27 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 import MenuButton from "./MenuCard.jsx";
-import { FaRegNewspaper, FaClipboardList, FaList, FaUserAlt } from 'react-icons/fa'
-import { BsFillChatFill, BsFillFileEarmarkSlidesFill } from 'react-icons/bs'
-import { RiOrganizationChart } from 'react-icons/ri'
-import { IoIosPeople } from 'react-icons/io'
+import { FaRegNewspaper, FaClipboardList, FaList, FaUserAlt } from "react-icons/fa";
+import { BsFillChatFill, BsFillFileEarmarkSlidesFill } from "react-icons/bs";
+import { RiOrganizationChart, RiEdit2Line } from "react-icons/ri";
+import { IoIosPeople } from "react-icons/io";
 
 const Backoffice = () => {
+  const roleUser = useSelector((state) => state.auth.user.roleId);
   return (
-    <div className='container-fluid p-5' style={{backgroundColor: '#edf2f7'}}>
-      <div className='row'>
-        <MenuButton title='Novedades' icon={<FaRegNewspaper />} to='backoffice/news'/>
-        <MenuButton title='Actividades' icon={<FaClipboardList />} to='backoffice/activities'/>
-        <MenuButton title='Categorias' icon={<FaList />} to='Categorias'/>
-        <MenuButton title='Testimonios' icon={<BsFillChatFill />} to='Testimonios'/>
-      </div>
-      <div className='row'>
-        <MenuButton title='Organizacion' icon={<RiOrganizationChart />} to='Organizacion'/>
-        <MenuButton title='Slides' icon={<BsFillFileEarmarkSlidesFill />} to='Slides'/>
-        <MenuButton title='Usuarios' icon={<FaUserAlt />} to='Usuarios'/>
-        <MenuButton title='Miembros' icon={<IoIosPeople />} to='Miembros'/>
-      </div>
-    </div>
-  )
-}
+    <>
+      {roleUser === 1 ? (
+        <div className="container-fluid p-5" style={{ backgroundColor: "#edf2f7" }}>
+          <div className="row">
+            <MenuButton title="Novedades" icon={<FaRegNewspaper />} to="backoffice/news" />
+            <MenuButton title="Actividades" icon={<FaClipboardList />} to="backoffice/activities" />
+            <MenuButton title="Categorias" icon={<FaList />} to="Categorias" />
+            <MenuButton title="Testimonios" icon={<BsFillChatFill />} to="Testimonios" />
+          </div>
+          <div className="row">
+            <MenuButton title="Organizacion" icon={<RiOrganizationChart />} to="Organizacion" />
+            <MenuButton title="Slides" icon={<BsFillFileEarmarkSlidesFill />} to="Slides" />
+            <MenuButton title="Usuarios" icon={<FaUserAlt />} to="Usuarios" />
+            <MenuButton title="Miembros" icon={<IoIosPeople />} to="Miembros" />
+          </div>
+        </div>
+      ) : (
+        <div className="container-fluid p-5" style={{ backgroundColor: "#edf2f7" }}>
+          <div className="row">
+            <MenuButton title="Editar Perfil" icon={<RiEdit2Line />} to="backoffice/edit" />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default Backoffice
+export default Backoffice;
