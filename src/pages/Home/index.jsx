4c8d7431c0afabbe9ab91d3ce77a896f3edd3ec8
NicoMunoz909 <nicolas.munoz@outlook.es>
2022-08-21 19:34:22 -0300

    Make testimonials section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,11 +10,19 @@ const Home = () => {
 
   const navigate = useNavigate()
   const [members, setMembers] = useState([])
+  const [testimonials, setTestimonials] = useState([])
 
   useEffect(() => {
     axios.get('http://localhost:3000/members')
     .then((res) => {
       setMembers(res.data);
+    })
+  }, [])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/testimonials')
+    .then((res) => {
+      setTestimonials(res.data);
     })
   }, [])
 
@@ -44,6 +52,24 @@ const Home = () => {
               <div className='card-img-overlay d-flex flex-column align-items-center justify-content-end text-center'>
                 <p className='text-light fw-bolder fs-5'>{member.name}</p>
                 <p className='text-light'>{member.role}</p>
+              </div>
+            </div>)
+          )}
+        </div>
+      </div>
+
+      <div className='mt-5'>
+        <div className='d-flex justify-content-between align-items-center'>
+          <h2 className='h2'>Testimonios</h2>
+          <Link to='testimonios'>Ver todos &gt;</Link>
+        </div>
+        <div className='row row-cols-5'>
+          {testimonials.map((testimonial) => (
+            <div key={testimonial.id} className='card border-0 col m-2 rounded-4' style={{backgroundColor: '#FAFA88'}}>
+              <img className='card-img-top w-50 mt-3 rounded-circle ' src={`http://localhost:3000/images/a7be39f693f676ce8650dfc1776ed33f`} alt={testimonial.imageUrl} />
+              <div className='card-body d-flex flex-column align-items-center justify-content-end text-center'>
+                <p className='fw-bolder fs-5'>{testimonial.name}</p>
+                <p className=''>{testimonial.content}</p>
               </div>
             </div>)
           )}
